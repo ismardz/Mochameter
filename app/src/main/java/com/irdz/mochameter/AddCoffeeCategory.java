571154d07899b25e67e78d043c793c71542b8f09
Ismael Rodríguez <ismael.rodriguez@saks.com>
2023-03-13 14:31:28 +0100

    Add coffee category register user activity

--- app/src/main/java/com/irdz/mochameter/AddCoffeeCategory.java
+++ app/src/main/java/com/irdz/mochameter/AddCoffeeCategory.java
@@ -2,13 +2,18 @@ package com.irdz.mochameter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.irdz.mochameter.dao.impl.UserDaoImpl;
 import com.irdz.mochameter.model.openfoodfacts.OpenFoodFactsResponse;
+import com.irdz.mochameter.service.CoffeeCreatorService;
 import com.irdz.mochameter.service.OpenFoodFactsService;
 import com.irdz.mochameter.ui.coffeedetail.CoffeeDetail;
+import com.irdz.mochameter.util.OpenFoodFactsUtils;
+import com.squareup.picasso.Picasso;
 
 import java.util.Optional;
 
@@ -30,8 +35,14 @@ public class AddCoffeeCategory extends AppCompatActivity {
                 TextView tvBrand = findViewById(R.id.tvBrand);
                 tvBrand.setText(p.brands);
 
+                ImageView ivCoffee = findViewById(R.id.ivCoffee);
+                Optional.ofNullable(OpenFoodFactsUtils.getImageUrl(productByBarcode))
+                    .map(Picasso.get()::load)
+                    .ifPresent(requestCreator -> requestCreator.into(ivCoffee));
+
                 findViewById(R.id.btnSendCategory).setOnClickListener(v -> {
                     OpenFoodFactsService.getInstance().addCoffeeCategory(p.code);
+                    CoffeeCreatorService.getInstance().insert(UserDaoImpl.getAndroidId(this), p.code);
                     finish();
                     Intent intent = new Intent(this, CoffeeDetail.class);
                     intent.putExtra("coffeeDetail", productByBarcode);
